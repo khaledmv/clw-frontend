6cd526c84619ded78b3c77cc9e3c17e52dd54c59
khaledmv <khaledhossionmv@gmail.com>
2026-07-11 14:16:01 +0600

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
 const nextConfig: NextConfig = {
   async rewrites() {
+    if (!apiUrl) {
+      console.warn("NEXT_PUBLIC_API_URL is not defined.");
+      return [];
+    }
+
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        destination: `${apiUrl}/api/:path*`,
       },
     ];
   },
